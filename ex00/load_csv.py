e85e250628bbe7780f6df60ebf58e7204d66711a
ijon <itan@student.42kl.edu.my>
2023-09-21 14:32:59 +0800

    done ex00

---
+++ ex00/load_csv.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    """load a csv file from path"""
+    try:
+        return pd.read_csv(path)
+    except FileNotFoundError:
+        print(f"File not found: {path}")
+        return pd.DataFrame()
