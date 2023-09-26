cc7432d5b7ce784241dcd98e7a322150f4d14d5c
ijon <itan@student.42kl.edu.my>
2023-09-26 20:54:22 +0800

    update

--- ex02/aff_pop.py
+++ ex02/aff_pop.py
@@ -26,18 +26,22 @@ def main():
         country1 = "Malaysia"
         country2 = "Singapore"
         years = np.array(data.columns[1:]).astype(int)
+        years = [i for i in years if i <= 2050]
         country1_data = data[data["country"] == country1].values[0, 1:]
         country1_data = [getvalue(i) for i in country1_data]
+        country1_data = country1_data[:len(years)]
         country2_data = data[data["country"] == country2].values[0, 1:]
         country2_data = [getvalue(i) for i in country2_data]
+        country2_data = country2_data[:len(years)]
 
         plt.plot(years, country1_data, color="green", label=country1)
         plt.plot(years, country2_data, color="blue", label=country2)
         plt.xlabel("Year")
         plt.ylabel("Population")
         plt.title("Population Projects")
-        plt.xticks(range(1800, 2081, 40))
-        plt.legend()
+        plt.yticks([20000000,40000000,60000000], ["20M","40M","60M"])
+        plt.xticks(range(1800, 2051, 40))
+        plt.legend(loc="lower right")
         plt.show()
     except Exception as e:
         print(e)
